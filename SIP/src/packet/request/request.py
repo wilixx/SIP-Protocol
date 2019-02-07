737a6b49443eccc03416ed45fc83b94a8a76137d
Trishant Pahwa <trishantpahwa@gmail.com>
2019-02-07 15:15:15 +0530

    Updated request class to create a packet

--- SIP/src/packet/request/request.py
+++ SIP/src/packet/request/request.py
@@ -1,13 +1,23 @@
 from packet import packet
 
+
 class request(packet):
     __packet_ = None
 
     __requests = ['INVITE', 'BYE', 'REGISTER', 'ACK']
 
-    def __init__(self):
-        self.__packet_ = packet.packet()
+    def __init__(self, request_type, sender_name, sender_network_name, domain,
+                 protocol, port, receiver_name, receiver_network_name, seq_num,
+                 subject, content_type, content_sub_type):
+        self.__packet_ = packet(request_type, sender_name, sender_network_name,
+                                domain, protocol, port, receiver_name,
+                                receiver_network_name, seq_num, subject,
+                                content_type, content_sub_type)
+        self.__add_uri(request_type, receiver_name, domain)
 
-    def __add_uri(self, status, receiver_name, domain):
-        self.__packet_ = status + '<sip:' + receiver_name + '@' + domain + ' SIP/2.0' + self.__packet
+    def __add_uri(self, request_type, receiver_name, domain):
+        self.__packet_ = request_type[0] + ' sip:' + receiver_name + \
+            '@' + domain + ' SIP/2.0' + self.__packet_.get_packet()
 
+    def get_request_packet(self):
+        return self.__packet_
