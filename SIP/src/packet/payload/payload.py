0cc3f5c4990c5afafe98963feaf27da275009d40
Trishant Pahwa <trishantpahwa@gmail.com>
2019-02-11 15:33:33 +0530

    Updated payload and tests to test packets and payloads

--- SIP/src/packet/payload/payload.py
+++ SIP/src/packet/payload/payload.py
@@ -1,37 +1,57 @@
 class payload:
     __payload = ''
 
-    def __init__(self):
-        self.__make_payload()
+    def __init__(self, version, sender_name, session_id, country_code,
+                 ip_type, domain, ip_address, media_type, protocol_id,
+                 media_transfer_type, protocol, port, modulation_type,
+                 modulation_port, session_name='SESSION SDP'):
+        self.__make_payload(version, sender_name, session_id, country_code,
+                            ip_type, domain, ip_address, media_type,
+                            protocol_id, media_transfer_type, protocol, port,
+                            modulation_type, modulation_port,
+                            session_name='SESSION SDP')
 
     def get_payload(self):
         return self.__payload
 
     def __add_version(self, version):
-        self.__payload += 'v=' + version
+        self.__payload += 'v=' + version + '\r\n'
 
-    def __add_owner_details(self, sender_name, session_id, country_code, ip_type, domain):
-        self.__payload += 'o=' + sender_name + ' ' + session_id + ' ' + country_code + ' ' + ip_type + ' ' + domain
+    def __add_owner_details(self, sender_name, session_id, country_code,
+                            ip_type, domain):
+        self.__payload += 'o=' + sender_name + ' ' + session_id + ' ' + \
+                          country_code + ' ' + ip_type + ' ' + domain + '\r\n'
 
     def __add_session_details(self, session_name='SESSION SDP'):
-        self.__payload += 's=' + session_name
+        self.__payload += 's=' + session_name + '\r\n'
 
     def __add_contact_details(self, country_code, ip_type, ip_address):
-        self.__packet += 'c=' + country_code + ' ' + ip_type + ' ' + ip_address
+        self.__payload += 'c=' + country_code + ' ' + ip_type + ' ' + \
+                          ip_address + '\r\n'
 
     def __add_time_details(self, start_time, stop_time):
-        self.__packet += 't=' + start_time + ' ' + stop_time
+        self.__payload += 't=' + start_time + ' ' + stop_time + '\r\n'
 
-    def __add_media_details(self, media_type, protocol_id, media_transfer_type):
-        self.__packet += 'm=' + media_type + ' ' + protocol_id + ' ' + media_transfer_type
+    def __add_media_details(self, media_type, protocol, media_transfer_type):
+        # Protocol ID
+        self.__payload += 'm=' + media_type + ' ' + protocol + ' ' + \
+                          media_transfer_type + '\r\n'
 
-    def __add_session_attributes(self, protocol, port, communication_port, transfer_port):
-        self.__packet += 'a=' + protocol + ':' + port + ' ' + communication_port + ':' + transfer_port
+    def __add_session_attributes(self, protocol, port, modulation_type,
+                                 modulation_port):
+        self.__payload += 'a=' + protocol + ':' + port + ' ' + \
+                         modulation_type + '/' + modulation_port
 
-    def __make_payload(self, version, sender_name, session_id, country_code, ip_type, domain, ip_address, media_type, protocol_id, media_transfer_type, protocol, port, communication_port, transfer_port, session_name='SESSION SDP'):
+    def __make_payload(self, version, sender_name, session_id, country_code,
+                       ip_type, domain, ip_address, media_type, protocol_id,
+                       media_transfer_type, protocol, port, communication_port,
+                       transfer_port, session_name='SESSION SDP'):
         self.__add_version(version)
-        self.__add_owner_details(sender_name, session_id, country_code, ip_type, domain)
-        self.__add_contact_details(country_code, ip_type, ip_address)
+        self.__add_owner_details(sender_name, session_id, country_code,
+                                 ip_type, domain)
+        self.__add_session_details(session_name)
         self.__add_time_details('0', '0')
+        self.__add_contact_details(country_code, ip_type, ip_address)
         self.__add_media_details(media_type, protocol_id, media_transfer_type)
-        self.__add_session_attributes(protocol, port, communication_port, transfer_port)
+        self.__add_session_attributes(protocol, port, communication_port,
+                                      transfer_port)
--- SIP/tests/packet_tests/packet_tests.py
+++ SIP/tests/packet_tests/packet_tests.py
@@ -26,7 +26,7 @@ class packet_test(unittest.TestCase):
         content_type = 'application'
         content_sub_type = 'sdp'
         tag = '123'
-        packet = 'INVITE sip:client1@there.com SIP/2.0\r\nVia SIP/2.0/TCP there.com:6050\r\nFrom: server1<sip:SERVER1@there.com>;tag=123\r\nTo: CLIENT1<sip:client1@there.com>;tag=123\r\nCallID: 39626979\r\nCSeq: 1 INVITE\r\nSubject: subject\r\nContact: client1<sip:CLIENT1@there.com>\r\nContent-Type: application/sdp'
+        sample_packet = 'INVITE sip:client1@there.com SIP/2.0\r\nVia SIP/2.0/TCP there.com:6050\r\nFrom: server1<sip:SERVER1@there.com>;tag=123\r\nTo: CLIENT1<sip:client1@there.com>;tag=123\r\nCallID: 39626979\r\nCSeq: 1 INVITE\r\nSubject: subject\r\nContact: client1<sip:CLIENT1@there.com>\r\nContent-Type: application/sdp'
         request_ = request(request_type, sender_name, sender_network_name,
                            domain, protocol, port, receiver_name,
                            receiver_network_name, seq_num,
@@ -37,10 +37,11 @@ class packet_test(unittest.TestCase):
             if 'CallID: ' in test:
                 test_cseq = test.split(': ')
                 test_cseq = test_cseq[1]
-        packet = packet.split('CallID: ')[0] + 'CallID: ' + test_cseq + '\r\n' + packet.split('CallID: ')[1][10:]
-        logger.log_print('\n' + 'Sample Request Packet: \r\n' + packet + '\n')
-        logger.log_print('\n' + 'Test Response Packet: \r\n' + test_packet + '\n')
-        self.assertEqual(request_.get_packet(), packet)
+        sample_packet = sample_packet.split('CallID: ')[0] + 'CallID: ' + \
+            test_cseq + '\r\n' + sample_packet.split('CallID: ')[1][10:]
+        logger.log_print('\n' + 'Sample Request Packet: \r\n' + sample_packet + '\n')
+        logger.log_print('\n' + 'Test Request Packet: \r\n' + test_packet + '\n')
+        self.assertEqual(request_.get_packet(), sample_packet)
 
     def test_response_packet(self):
         code = '100'
@@ -57,7 +58,7 @@ class packet_test(unittest.TestCase):
         content_type = 'application'
         content_sub_type = 'sdp'
         tag = '123'
-        packet = 'SIP/2.0 100 TRYING\r\nVia SIP/2.0/TCP there.com:6050\r\nFrom: server1<sip:SERVER1@there.com>;tag=123\r\nTo: CLIENT1<sip:client1@there.com>;tag=123\r\nCallID: 90815289\r\nCSeq: 1 INVITE\r\nSubject: subject\r\nContact: client1<sip:CLIENT1@there.com>\r\nContent-Type: application/sdp'
+        sample_packet = 'SIP/2.0 100 TRYING\r\nVia SIP/2.0/TCP there.com:6050\r\nFrom: server1<sip:SERVER1@there.com>;tag=123\r\nTo: CLIENT1<sip:client1@there.com>;tag=123\r\nCallID: 90815289\r\nCSeq: 1 INVITE\r\nSubject: subject\r\nContact: client1<sip:CLIENT1@there.com>\r\nContent-Type: application/sdp'
         response_ = response(code, sender_name, sender_network_name, domain,
                              protocol, port, receiver_name,
                              receiver_network_name, seq_num, request_type,
@@ -68,13 +69,38 @@ class packet_test(unittest.TestCase):
             if 'CallID: ' in test:
                 test_cseq = test.split(': ')
                 test_cseq = test_cseq[1]
-        packet = packet.split('CallID: ')[0] + 'CallID: ' + test_cseq + '\r\n' + packet.split('CallID: ')[1][10:]
-        logger.log_print('\n' + 'Sample Response Packet: \r\n' + packet + '\n')
+        sample_packet = sample_packet.split('CallID: ')[0] + 'CallID: ' + \
+            test_cseq + '\r\n' + sample_packet.split('CallID: ')[1][10:]
+        logger.log_print('\n' + 'Sample Response Packet: \r\n' + sample_packet + '\n')
         logger.log_print('\n' + 'Test Response Packet: \r\n' + test_packet + '\n')
-        self.assertEqual(test_packet, packet)
+        self.assertEqual(test_packet, sample_packet)
 
     def test_payload(self):
-        payload_ = payload()
+        version = '0'
+        sender_name = 'server1'
+        session_id = '0000776624 9563745610'
+        country_code = 'IN'
+        ip_type = 'IP4'
+        domain = 'there.com'
+        ip_address = '10.6.29.100'
+        media_type = 'audio'
+        protocol_id = '49712'
+        media_transfer_type = 'RTP/AVP'
+        protocol = 'rtpmap'
+        port = '0'
+        communication_port = 'PCMU'
+        transfer_port = '8000'
+        session_name = 'SESSION SDP'
+        sample_payload = 'v=0\r\no=server1 0000776624 9563745610 IN IP4 there.com\r\ns=SESSION SDP\r\nt=0 0\r\nc=IN IP4 10.6.29.100\r\nm=audio 49712 RTP/AVP\r\na=rtpmap:0 PCMU/8000'
+        test_payload = payload(version, sender_name, session_id, country_code,
+                               ip_type, domain, ip_address, media_type,
+                               protocol_id, media_transfer_type, protocol,
+                               port, communication_port, transfer_port,
+                               session_name)
+        test_payload = test_payload.get_payload()
+        logger.log_print('\n' + 'Sample Payload: \r\n' + sample_payload + '\n')
+        logger.log_print('\n' + 'Test Payload: \r\n' + test_payload + '\n')
+        self.assertEqual(sample_payload, test_payload)
 
 if __name__ == '__main__':
     logger.get_log_file_name('packet_test_log.log')
